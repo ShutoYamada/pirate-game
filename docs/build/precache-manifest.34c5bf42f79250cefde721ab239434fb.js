4845b268a2b654e1019a2eb9b4922a4286257d32
ShutoYamada <stym1122@gmail.com>
2020-07-08 21:11:13 +0900

    再ビルド

---
+++ docs/build/precache-manifest.34c5bf42f79250cefde721ab239434fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171f16222b493fa9b569db784791a091",
+    "url": "/pirate-game/index.html"
+  },
+  {
+    "revision": "d826699f6b50a0de5b66",
+    "url": "/pirate-game/static/css/main.42ee559f.chunk.css"
+  },
+  {
+    "revision": "d56cccb31c961c20855a",
+    "url": "/pirate-game/static/js/2.33452fcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pirate-game/static/js/2.33452fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d826699f6b50a0de5b66",
+    "url": "/pirate-game/static/js/main.94a3269f.chunk.js"
+  },
+  {
+    "revision": "eaaa2e14361f24ce1745",
+    "url": "/pirate-game/static/js/runtime-main.4464a396.js"
+  }
+]);
